2ee1e7328967e5c6cabe0262e4e2d2daddbc86de
Jeff Daily <jeff.daily@amd.com>
2019-06-10 18:02:24 +0000

    release async ops without signals after signal wait

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -4635,6 +4635,10 @@ HSADispatch::waitComplete() {
         }
 
         DBOUT (DB_MISC, "complete!\n");
+
+        // clear reference counts for signal-less ops.
+        asyncOpsWithoutSignal.clear();
+
     } else {
         // Some commands may have null signal - in this case we can't actually
         // track their status so assume they are complete.
@@ -4978,6 +4982,9 @@ HSABarrier::waitComplete() {
         depAsyncOps[i] = nullptr;
     }
 
+    // clear reference counts for signal-less ops.
+    asyncOpsWithoutSignal.clear();
+
     return _wait_complete_status;
 }
 
@@ -5221,6 +5228,9 @@ HSACopy::waitComplete() {
 
     // clear reference counts for dependent ops.
     depAsyncOp = nullptr;
+
+    // clear reference counts for signal-less ops.
+    asyncOpsWithoutSignal.clear();
 
     return _wait_complete_status;
 }
